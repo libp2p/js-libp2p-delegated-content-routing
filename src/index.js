c8f40e2ba4a863fc7edf77190be70e3cef74dc18
dignifiedquire <dignifiedquire@gmail.com>
2018-04-19 14:28:40 +0200

    feat: implement provide

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 
 const PeerInfo = require('peer-info')
 const dht = require('ipfs-api/src/dht')
+const swarm = require('ipfs-api/src/swarm')
+const refs = require('ipfs-api/src/refs')
 const defaultConfig = require('ipfs-api/src/utils/default-config')
+const series = require('async/series')
+const parallel = require('async/parallel')
+const reflect = require('async/reflect')
+const multiaddr = require('multiaddr')
 
 const DEFAULT_IPFS_API = {
   protocol: 'https',
@@ -10,18 +16,40 @@ const DEFAULT_IPFS_API = {
   host: 'ipfs.io'
 }
 
+const DEFAULT_BOOSTRAP_NODES = [
+  '/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
+  '/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
+  '/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
+  '/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
+  '/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
+  '/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
+  '/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+  '/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6'
+]
+
 /**
  * An implementation of content routing, using a delegated peer.
  */
 class DelegatedContentRouting {
   /**
-   * Create a new KadDHT.
+   * Create a new DelegatedContentRouting instance.
    *
-   * @param {object} api - the api endpoint of the delegated node to use.
+   * @param {PeerInfo} peerInfo - the node that is using this routing.
+   * @param {object} [api] - the api endpoint of the delegated node to use.
+   * @param {Array<Multiaddr>} [bootstrappers] - list of bootstrapper nodes we are connected to.
    */
-  constructor (api) {
+  constructor (peerInfo, api, bootstrappers) {
+    if (peerInfo == null) {
+      throw new Error('missing self peerInfo')
+    }
+
     this.api = Object.assign({}, defaultConfig(), api || DEFAULT_IPFS_API)
     this.dht = dht(this.api)
+    this.swarm = swarm(this.api)
+    this.refs = refs(this.api)
+
+    this.peerInfo = peerInfo
+    this.bootstrappers = bootstrappers || DEFAULT_BOOSTRAP_NODES.map((addr) => multiaddr(addr))
   }
 
   /**
@@ -61,13 +89,37 @@ class DelegatedContentRouting {
   /**
    * Announce to the network that the delegated node can provide the given key.
    *
+   * Currently this uses the following hack
+   * - call swarm.connect on the delegated node to us, to ensure we are connected
+   * - call refs --recursive on the delegated node, so it fetches the content
+   *
    * @param {CID} key
    * @param {function(Error)} callback
    * @returns {void}
    */
   provide (key, callback) {
-    // TODO: Implement me
-    callback()
+    const addrs = this.bootstrappers.map((addr) => {
+      return addr.encapsulate(`/p2p-circuit/ipfs/${this.peerInfo.id}`)
+    })
+    series([
+      // TODO: do we want to connect through all of them?
+      (cb) => parallel(addrs.map((addr) => {
+        return reflect((cb) => this.swarm.connect(addr.toString(), cb))
+      }), (err, results) => {
+        if (err) {
+          return cb(err)
+        }
+        // only some need to succeed
+        const success = results.filter((res) => res.error == null)
+        if (success.length === 0) {
+          return cb(new Error('unable to swarm.connect using p2p-circuit'))
+        }
+        cb()
+      }),
+      (cb) => {
+        this.refs(key.toBaseEncodedString(), {recursive: true}, cb)
+      }
+    ], (err) => callback(err))
   }
 }
 
