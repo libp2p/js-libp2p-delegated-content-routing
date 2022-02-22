40b4ce36489f84cefaedabbcfd64a3fb9bc3a100
achingbrain <alex@achingbrain.net>
2022-02-22 06:57:53 +0200

    feat: convert to typescript
    - Converts to typescript
    - Only named exports
    - No more CJS, only ESM
    - Runs tests on all supported environments
    - Adds auto-publish
    - Adds dependabot
    
    BREAKING CHANGE: switch to named exports, ESM only

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
-# js-libp2p-delegated-content-routing
+# js-libp2p-delegated-content-routing <!-- omit in toc -->
 
 [![](https://img.shields.io/badge/made%20by-Protocol%20Labs-blue.svg?style=flat-square)](http://protocol.ai)
 [![](https://img.shields.io/badge/project-libp2p-yellow.svg?style=flat-square)](http://libp2p.io/)
 [![](https://img.shields.io/badge/freenode-%23libp2p-yellow.svg?style=flat-square)](http://webchat.freenode.net/?channels=%23libp2p)
 [![Discourse posts](https://img.shields.io/discourse/https/discuss.libp2p.io/posts.svg)](https://discuss.libp2p.io)
 [![](https://img.shields.io/codecov/c/github/libp2p/js-libp2p-delegated-content-routing.svg?style=flat-square)](https://codecov.io/gh/libp2p/js-libp2p-delegated-content-routing)
-[![](https://img.shields.io/travis/libp2p/js-libp2p-delegated-content-routing.svg?style=flat-square)](https://travis-ci.com/libp2p/js-libp2p-delegated-content-routing)
+[![Build Status](https://github.com/libp2p/js-libp2p-delegated-content-routing/actions/workflows/js-test-and-release.yml/badge.svg?branch=main)](https://github.com/libp2p/js-libp2p-delegated-content-routing/actions/workflows/js-test-and-release.yml)
 [![Dependency Status](https://david-dm.org/libp2p/js-libp2p-delegated-content-routing.svg?style=flat-square)](https://david-dm.org/libp2p/js-libp2p-delegated-content-routing)
 
 Leverage other peers in the network to perform Content Routing calls.
 
 Requires access to `/api/v0/dht/findprovs` and `/api/v0/refs` HTTP API endpoints of the delegate node.
 
-## Lead Maintainer
+## Table of contents <!-- omit in toc -->
 
-[Jacob Heun](https://github.com/jacobheun)
+- [Requirements](#requirements)
+- [Example](#example)
+- [License](#license)
+  - [Contribution](#contribution)
 
 ## Requirements
 
-`libp2p-delegated-content-routing` leverages the `ipfs-http-client` library and requires an instance of it as a constructor argument.
+`@libp2p/delegated-content-routing` leverages the `ipfs-http-client` library and requires an instance of it as a constructor argument.
 
 ```sh
-npm install ipfs-http-client libp2p-delegated-content-routing
+npm install ipfs-http-client @libp2p/delegated-content-routing
 ```
 
 ## Example
 
 ```js
-const DelegatedContentRouting = require('libp2p-delegated-content-routing')
-const ipfsHttpClient = require('ipfs-http-client')
+import { DelegatedContentRouting } from '@libp2p/delegated-content-routing'
+import ipfsHttpClient from 'ipfs-http-client'
 
 // default is to use ipfs.io
 const routing = new DelegatedContentRouting(peerId, ipfsHttpClient.create({
@@ -49,4 +52,11 @@ console.log('providing %s', cid.toBaseEncodedString())
 
 ## License
 
-MIT
+Licensed under either of
+
+ * Apache 2.0, ([LICENSE-APACHE](LICENSE-APACHE) / http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT ([LICENSE-MIT](LICENSE-MIT) / http://opensource.org/licenses/MIT)
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,26 +1,29 @@
 /* eslint-env mocha */
-'use strict'
 
-const { expect } = require('aegir/utils/chai')
-const { createFactory } = require('ipfsd-ctl')
-const ipfsHttpClient = require('ipfs-http-client')
-const { CID } = ipfsHttpClient
-const PeerId = require('peer-id')
-const all = require('it-all')
-const drain = require('it-drain')
-const { isNode } = require('ipfs-utils/src/env')
-const { fromString: uint8ArrayFromString } = require('uint8arrays/from-string')
+import { expect } from 'aegir/utils/chai.js'
+import { Controller, createFactory } from 'ipfsd-ctl'
+import { create, CID } from 'ipfs-http-client'
+import all from 'it-all'
+import drain from 'it-drain'
+import { isNode } from 'wherearewe'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
+import { DelegatedContentRouting } from '../src/index.js'
+// @ts-expect-error no types
+import goIpfs from 'go-ipfs'
+import { peerIdFromString } from '@libp2p/peer-id'
+import type { PeerId } from '@libp2p/interfaces/peer-id'
+import type { IDResult } from 'ipfs-core-types/src/root'
+import type { PeerData } from 'ipfs-core-types/src/dht/index.js'
+
 const factory = createFactory({
   type: 'go',
-  ipfsHttpModule: require('ipfs-http-client'),
-  ipfsBin: isNode ? require('go-ipfs').path() : undefined,
+  ipfsHttpModule: { create },
+  ipfsBin: isNode ? goIpfs.path() : undefined,
   test: true,
   endpoint: 'http://localhost:57483'
 })
 
-const DelegatedContentRouting = require('../src')
-
-async function spawnNode (bootstrap = []) {
+async function spawnNode (bootstrap: any[] = []) {
   const node = await factory.spawn({
     // Lock down the nodes so testing can be deterministic
     ipfsOptions: {
@@ -46,11 +49,11 @@ async function spawnNode (bootstrap = []) {
 describe('DelegatedContentRouting', function () {
   this.timeout(20 * 1000) // we're spawning daemons, give ci some time
 
-  let selfNode
-  let selfId
-  let delegateNode
-  let bootstrapNode
-  let bootstrapId
+  let selfNode: Controller
+  let selfId: PeerId
+  let delegateNode: Controller
+  let bootstrapNode: Controller
+  let bootstrapId: IDResult
 
   before(async () => {
     // Spawn a "Boostrap" node that doesnt connect to anything
@@ -61,39 +64,36 @@ describe('DelegatedContentRouting', function () {
     // Spawn our local node and bootstrap the bootstrapper node
     const self = await spawnNode(bootstrapId.addresses)
     selfNode = self.node
-    selfId = PeerId.parse(self.id.id)
+    selfId = peerIdFromString(self.id.id)
 
     // Spawn the delegate node and bootstrap the bootstrapper node
     const delegate = await spawnNode(bootstrapId.addresses)
     delegateNode = delegate.node
   })
 
-  after(() => {
-    return factory.clean()
+  after(async () => {
+    return await factory.clean()
   })
 
   describe('create', () => {
-    it('should require peerInfo', () => {
+    it('should require ipfs http client', () => {
+      // @ts-expect-error missing parameters
       expect(() => new DelegatedContentRouting()).to.throw()
     })
 
-    it('should require ipfs http client', () => {
-      expect(() => new DelegatedContentRouting(selfId)).to.throw()
-    })
-
     it('should accept an http api client instance at construction time', () => {
-      const client = ipfsHttpClient.create({
+      const client = create({
         protocol: 'http',
         port: 8000,
         host: 'localhost'
       })
-      const router = new DelegatedContentRouting(selfId, client)
+      const router = new DelegatedContentRouting(client)
 
-      expect(router).to.have.property('_client')
+      expect(router).to.have.property('client')
         .that.has.property('getEndpointConfig')
         .that.is.a('function')
 
-      expect(router._client.getEndpointConfig()).to.deep.include({
+      expect(client.getEndpointConfig()).to.deep.include({
         protocol: 'http:',
         port: '8000',
         host: 'localhost'
@@ -118,38 +118,52 @@ describe('DelegatedContentRouting', function () {
 
     it('should be able to find providers through the delegate node', async function () {
       const opts = delegateNode.apiAddr.toOptions()
-      const routing = new DelegatedContentRouting(selfId, ipfsHttpClient.create({
+      const routing = new DelegatedContentRouting(create({
         protocol: 'http',
         port: opts.port,
         host: opts.host
       }))
 
-      const providers = await all(routing.findProviders(cid, { numProviders: 2 }))
+      const events = await all(routing.findProviders(cid))
+      const providers: PeerData[] = []
+
+      for (const event of events) {
+        if (event.name === 'PEER_RESPONSE') {
+          providers.push(...event.providers)
+        }
+      }
 
       // We should get the bootstrap node as provider
       // The delegate node is not included, because it is handling the requests
-      expect(providers.map((p) => p.id.toB58String())).to.include(bootstrapId.id, 'Did not include bootstrap node')
-      expect(providers.map((p) => p.id.toB58String())).to.include(selfId.toB58String(), 'Did not include self node')
+      expect(providers.map((p) => p.id.toString())).to.include(bootstrapId.id, 'Did not include bootstrap node')
+      expect(providers.map((p) => p.id.toString())).to.include(selfId.toString(), 'Did not include self node')
     })
 
     it('should be able to specify a timeout', async () => {
       const opts = delegateNode.apiAddr.toOptions()
-      const routing = new DelegatedContentRouting(selfId, ipfsHttpClient.create({
+      const routing = new DelegatedContentRouting(create({
         protocol: 'http',
         port: opts.port,
         host: opts.host
       }))
 
-      const providers = await all(routing.findProviders(cid, { numProviders: 2, timeout: 5e3 }))
+      const events = await all(routing.findProviders(cid, { timeout: 5e3 }))
+      const providers: PeerData[] = []
 
-      expect(providers.map((p) => p.id.toB58String())).to.include(bootstrapId.id, 'Did not include bootstrap node')
+      for (const event of events) {
+        if (event.name === 'PEER_RESPONSE') {
+          providers.push(...event.providers)
+        }
+      }
+
+      expect(providers.map((p) => p.id.toString())).to.include(bootstrapId.id, 'Did not include bootstrap node')
     })
   })
 
   describe('provide', () => {
     it('should be able to register as a content provider to the delegate node', async () => {
       const opts = delegateNode.apiAddr.toOptions()
-      const contentRouter = new DelegatedContentRouting(selfId, ipfsHttpClient.create({
+      const contentRouter = new DelegatedContentRouting(create({
         protocol: 'http',
         port: opts.port,
         host: opts.host
@@ -159,28 +173,43 @@ describe('DelegatedContentRouting', function () {
 
       await contentRouter.provide(cid)
 
-      const providers = await all(delegateNode.api.dht.findProvs(cid, { numProviders: 2 }))
+      const providers: PeerData[] = []
+
+      for await (const event of delegateNode.api.dht.findProvs(cid)) {
+        if (event.name === 'PEER_RESPONSE') {
+          providers.push(...event.providers)
+        }
+      }
 
       // We are hosting the file, validate we're the provider
-      expect(providers.map((p) => p.id)).to.include(selfId.toB58String(), 'Did not include self node')
+      expect(providers.map((p) => p.id)).to.include(selfId.toString(), 'Did not include self node')
     })
 
     it('should provide non-dag-pb nodes via the delegate node', async () => {
       const opts = delegateNode.apiAddr.toOptions()
-      const contentRouter = new DelegatedContentRouting(selfId, ipfsHttpClient.create({
+      const contentRouter = new DelegatedContentRouting(create({
         protocol: 'http',
         port: opts.port,
         host: opts.host
       }))
 
-      const cid = await selfNode.api.dag.put(`hello-${Math.random()}`, { format: 'dag-cbor', hashAlg: 'sha2-256' })
+      const cid = await selfNode.api.dag.put(`hello-${Math.random()}`, {
+        storeCodec: 'dag-cbor',
+        hashAlg: 'sha2-256'
+      })
 
       await contentRouter.provide(cid)
 
-      const providers = await all(delegateNode.api.dht.findProvs(cid, { numProviders: 2 }))
+      const providers: PeerData[] = []
+
+      for await (const event of delegateNode.api.dht.findProvs(cid)) {
+        if (event.name === 'PEER_RESPONSE') {
+          providers.push(...event.providers)
+        }
+      }
 
       // We are hosting the file, validate we're the provider
-      expect(providers.map((p) => p.id)).to.include(selfId.toB58String(), 'Did not include self node')
+      expect(providers.map((p) => p.id)).to.include(selfId.toString(), 'Did not include self node')
     })
   })
 })
